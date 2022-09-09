df8aef141f02fca2c7e0684d4efe604a586d3d79
Adam <inmon.adam@yahoo.com>
2022-09-08 21:23:06 -0500

    commit tree 607f0b70d3eb8eade8b6400e9cf96e4b1cad5e69

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,37 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name] 
+      Student.to_s(params[:name])
+    else
+      Student.all
+    end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 end
+
+
+
+class Movie < ActiveRecord::Base
+
+  def index
+    movies = Movie.all
+    render json: movies
+  end
+
+  def show
+    movies = if params[:name] 
+      Movie.where(params[:name])
+    else
+      Movie.all
+    end
+    render json: students
+  end
+
+end 
